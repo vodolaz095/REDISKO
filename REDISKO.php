7cdb2b1a9093cd84067e2d3c71a17f8659025fa1
Anatoly Ostroumov <ostroumov095@gmail.com>
2012-05-19 00:46:30 +0400

    minor bug with command log firmat is corrected

--- REDISKO.php
+++ REDISKO.php
@@ -103,7 +103,7 @@ class REDIS
                         $type='Empty';
                     }
 
-                $this->stats[]=array('command'=> trim($name.implode(' ',$args)),
+                $this->stats[]=array('command'=> trim(implode(' ',$args)),
                                      'time'   =>(1000*round((microtime(true)-$start), 6)),
                                      'type'   =>$type);
                 return $a;
